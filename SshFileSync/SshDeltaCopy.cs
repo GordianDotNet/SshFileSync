cfe7c79a5eb789116468664e0212bf464d533ff0
Gordian <gordian.cs@web.de>
2017-07-15 23:06:57 +0200

    MIT License update

--- SshFileSync/SshDeltaCopy.cs
+++ SshFileSync/SshDeltaCopy.cs
@@ -1,5 +1,31 @@
-﻿// Copyright (c) 2017 GordianDotNet (https://github.com/GordianDotNet/SshFileSync)
-// 20170715: Version 1.2.0
+﻿/* 
+20170715: Version 1.2.0
+
+MIT License
+
+Copyright (c) 2017 GordianDotNet (https://github.com/GordianDotNet/SshFileSync)
+Copyright (c) 2012-2016, RENCI (https://www.nuget.org/packages/SSH.NET and https://github.com/sshnet/SSH.NET)
+Copyright (c) 2014 Adam Hathcock (https://www.nuget.org/packages/sharpcompress/ and https://github.com/adamhathcock/sharpcompress)
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
